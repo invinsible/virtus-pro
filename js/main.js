b8de9b6f2f011669c1577e38302cada19ed23017
Sergey Butyanov <igor@tomatstudio.ru>
2020-12-10 10:19:32 +0300

    Tabs

--- js/main.js
+++ js/main.js
@@ -11,8 +11,41 @@ menuBtn.addEventListener('click', function(e){
   header.classList.toggle('menu-bg');
 });
 
+// Search
 searchBtn.addEventListener('click', function(e){
   e.preventDefault();
   const item = document.querySelector('.search__item');
   item.classList.toggle('active');
 });
+
+// Tabs
+const tab = function() {
+  let tabNav = document.querySelectorAll('.tabBtn');
+  let tabContent = document.querySelectorAll('.tabContent');
+  let currentTab;
+
+  tabNav.forEach(item => {
+    item.addEventListener('click', selectTabNav);
+  });
+
+  // Handler for tab-btn
+  function selectTabNav(e) {
+    e.preventDefault();
+    tabNav.forEach(item => {
+      item.classList.remove('active');      
+    });
+    this.classList.add('active');
+
+    currentTab = this.getAttribute('data-tab-name');
+    selectTabContent(currentTab);
+  };
+
+  // Handler for tab-content
+  function selectTabContent(el) {
+    tabContent.forEach(item => {
+      item.classList.contains(el) ? item.classList.add('active') : item.classList.remove('active');
+    });
+  }
+};
+
+tab();
